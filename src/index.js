4117c717b432dfcb48085c5ed2aa31abdbd320ba
Abszissex <rubSXkwZDZGqrr4>
2022-04-24 14:56:28 +0200

    added typescript

--- src/index.js
+++
@@ -1,6 +0,0 @@
-const myFunc = (a, b) => {
-
-    return a + b;
-}
-
-console.log(myFunc(2, 3));
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { add } from "./helper";
+const result = add(2, 3) + add(4, 5);
+console.log(`The result: ${result}`);
